469a620ab3897e800fe23144a90f1cc3e07eb34d
Volodymyr Kozhukhar <kozhukharvladimir@gmail.com>
2026-09-09 17:19:52 +0200

    Add skill lookup and deletion routes

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ type Skill = {
     title: string,
 }
 
-const skills: Skill[] = [];
+let skills: Skill[] = [];
 let nextSkillId: number = 1;
 
 function isCreateSkillInput(value: unknown): value is CreateSkillInput {
@@ -24,6 +24,7 @@ function isCreateSkillInput(value: unknown): value is CreateSkillInput {
 const app = express();
 app.use(express.json());
 
+//GET
 app.get("/", (request, response) => {
     response.send("Node server is running");
 });
@@ -36,6 +37,22 @@ app.get("/skills", (request, response) => {
     response.json(skills);
 });
 
+app.get("/skills/:id", (request, response) => {
+    const id = Number(request.params.id);
+    
+    if(Number.isInteger(id) && id > 0){
+        const skill = skills.find(skill => skill.id === id);
+        if(skill === undefined){
+            response.status(404).json({"error": `No skill with id:${id}`});
+            return;
+        }
+        response.json(skill);
+        return;
+    }
+    response.status(400).json({"error": "Invalid skill id"});
+});
+
+//POST
 app.post("/skills", (request, response) => {
     const body: unknown = request.body;
     if(isCreateSkillInput(body)){
@@ -48,6 +65,24 @@ app.post("/skills", (request, response) => {
     response.status(400).json({ "error": "Invalid skill input" });
 });
 
+//DELETE
+app.delete("/skills/:id", (request, response) => {
+    const id = Number(request.params.id);
+
+    if(Number.isInteger(id) && id > 0){
+        const skill = skills.find(skill => skill.id === id);
+        if(skill === undefined){
+            response.status(404).json({"error": `No skill with id:${id}`});
+            return;
+        }
+        skills = skills.filter(skill => skill.id !== id);
+        response.status(204).send();
+        return;
+    }
+    response.status(400).json({ "error": "Invalid skill id" });
+});
+
+// Callback
 app.use((request, response) => {
     response.status(404).json({"error": "Not found"});
 });
